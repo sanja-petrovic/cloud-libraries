357a7af44cedb84b336a4bfb7853722b4ec831f9
Sanja Petrović <sanjica.petrovic7@gmail.com>
2024-01-10 11:54:54 +0100

    feat: k8s setup

--- central/database.py
+++ central/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
 
+time.sleep(30)
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/central_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- local/database.py
+++ local/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import time
+
+time.sleep(30)
 
 SQLALCHEMY_DATABASE_URL = (
     f'postgresql://postgres:postgres@postgres:5432/{os.environ["DB"]}'
